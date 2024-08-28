24e9b51ad027b442038681345b4f19459e182b9d
Yiyu Qian <yiyuqianunitediting@gmail.com>
2024-08-28 21:27:04 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploy-github/",
+  base: "/React-Quiz-App/", // github repo name
   plugins: [react()],
 })
